0d03938336843e66886903116584d4ead2925749
IS2511 <is2511@bk.ru>
2023-05-02 06:56:39 +0300

    feat(chat): liptogod new badge, again

--- v2/script.js
+++ v2/script.js
@@ -1,4 +1,4 @@
-const version = '2.28.6+497';
+const version = '2.28.6+450';
 
 function* entries(obj) {
     for (let key of Object.keys(obj)) {
@@ -1011,9 +1011,12 @@ var Chat = {
                         // $badge.attr('src', 'https://media.discordapp.net/attachments/987445091893403658/987445194578354186/ezgif72_5.png');
                         // // https://media.discordapp.net/attachments/987445091893403658/987445194318299266/ezgif36_5.png
                         // // https://media.discordapp.net/attachments/987445091893403658/987445194087604224/ezgif18_5.png
-                        $badge.attr('src', 'https://cdn.7tv.app/emote/60e8677677b18d5dd3800410/3x.webp');
-                        // https://cdn.7tv.app/emote/60e8677677b18d5dd3800410/2x.webp
-                        // https://cdn.7tv.app/emote/60e8677677b18d5dd3800410/1x.webp
+                        // $badge.attr('src', 'https://cdn.7tv.app/emote/60e8677677b18d5dd3800410/3x.webp');
+                        // // https://cdn.7tv.app/emote/60e8677677b18d5dd3800410/2x.webp
+                        // // https://cdn.7tv.app/emote/60e8677677b18d5dd3800410/1x.webp
+                        $badge.attr('src', 'https://cdn.7tv.app/emote/61d0e186ce8bd4a59cb895af/3x.webp');
+                        // https://cdn.7tv.app/emote/61d0e186ce8bd4a59cb895af/2x.webp
+                        // https://cdn.7tv.app/emote/61d0e186ce8bd4a59cb895af/1x.webp
                     }
                     if (nick.toLowerCase() === 'itsbandorax') {
                         $badge.attr('src', 'https://cdn.7tv.app/emote/62b069136f979a87147499f9/3x.webp');
